a56fc59cf75e7d0c46525339ffdb03058353c35e
Ceara Zhang <cearazhang@Cearas-MacBook-Pro.local>
2024-03-22 11:46:53 -0400

    new clause

--- spotify_graph.py
+++ spotify_graph.py
@@ -9,19 +9,27 @@ password = "neo4j"
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
 
+# Connect to Neo4j
+uri = "neo4j://localhost:7687"
+user = "neo4j"
+password = "neo4j"
+driver = GraphDatabase.driver(uri, auth=(user, password))
+
+
 # Function to recommend similar songs for a given song
 def recommend_similar_songs(track_id):
     with driver.session() as session:
         # Cypher query to find similar songs for a given song
         query = (
             "MATCH (s1:Track {track_id: $track_id})-[:SIMILAR_TO]->(s2:Track) "
+            "WHERE s1.artists <> s2.artists "
             "RETURN s2.track_name AS similar_song, s2.artists AS artist "
             "LIMIT 5"
         )
         result = session.run(query, track_id=track_id)
         recommendations = [(record["similar_song"], record["artist"]) for record in result]
         return recommendations
-
+1
 # usage
 track = "3qehmGY1DnlBI5W9iFlUhR"
 recs = recommend_similar_songs(track)
