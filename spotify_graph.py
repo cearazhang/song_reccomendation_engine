22d3852ac3d150f12cdd1ff6a7bc5c969f5d4513
gellerjulia <74266997+gellerjulia@users.noreply.github.com>
2024-03-22 15:16:12 -0400

    asdf

--- spotify_graph.py
+++ spotify_graph.py
@@ -13,8 +13,13 @@ def execute_query(query):
     with driver.session() as session:
         session.run(query)
 
-# Function to import nodes
 def import_nodes(df):
+    """
+    Imports all song nodes from the given dataframe into neo4j.
+
+    Args:
+        df (DataFrame): DataFrame of songs
+    """
     df = df.copy()
     # remove duplicate songs
     df = df[['track_id', 'track_name', 'artists']].drop_duplicates()
@@ -23,27 +28,37 @@ def import_nodes(df):
         track_name = row['track_name']
         artists = row['artists']
         query = f"CREATE (:Song {{track_id: '{track_id}', track_name: '{track_name}', artists: '{artists}'}})"
-    execute_query(query)
+        execute_query(query)
 
-# Function to import edges
 def import_edges(df):
+    """
+    Imports all song edges from the given dataframe into neo4j.
+
+    Args:
+        df (DataFrame): DataFrame of songs and their similarity scores.
+    """
     for _, row in df.iterrows():
         track_id1 = row['track_id1']
         track_id2 = row['track_id2']
         sim_score= row['sim_score']
         query = f"MATCH (a:Song {{track_id: '{str(track_id1)}'}}), (b:Song {{track_id: '{str(track_id2)}'}}) CREATE (a)-[:Similar {{score: toFloat({sim_score})}}]->(b)"
-    execute_query(query)
+        execute_query(query)
 
-# Function to recommend similar songs for a given song
 def recommend_similar_songs(track_id):
-    query = f"MATCH (source {{track_id:'{str(track_id)}'}})-[r:SIMILAR_TO]->(target) WITH DISTINCT target, r.score AS score RETURN target.track_id AS track_id, target.track_name AS track_name, target.artists AS artists ORDER BY score DESC LIMIT 5"
+    """
+    Reccommends top 5 songs similar to given track.
+
+    Args:
+        track_id (str): id of the track to find similar songs to.
+    """
+    query =   f"MATCH (source {{track_id:'{track_id}'}})-[r:Similar]->(target) WITH DISTINCT target, r.score AS score RETURN target.track_id AS track_id, target.track_name AS track_name, target.artists AS artists ORDER BY score DESC LIMIT 5"
     with driver.session() as session:
         result = session.run(query)
         result = list(result)
     recommendations = [(record["track_name"], record["artists"]) for record in result]
     return recommendations
 
-# import song and similairty data
+# import songs and similarity data
 sample_song_df = pd.read_csv('sample_songs.csv')
 sample_song_sim_df =  pd.read_csv('sample_song_similarity.csv')
 
@@ -52,7 +67,7 @@ import_nodes(sample_song_df)
 import_edges(sample_song_sim_df)
 
 # usage
-track = "1J17nBsbh9HWVvtJFsAPYb"
+track = "0wnLMEOQlyW3Es1ag0HMVV"
 recs = recommend_similar_songs(track)
 print("Recommended songs for", track, "are: ", recs)
 
