3b94b450f20c284902c164380496ca1e2d48b4ab
Ceara Zhang <cearazhang@Cearas-MacBook-Pro.local>
2024-03-22 09:21:42 -0400

    connect to neo4j

--- spotify_graph.py
+++ spotify_graph.py
@@ -2,17 +2,31 @@ import pandas as pd
 from neo4j import GraphDatabase
 
 
-
 # Connect to Neo4j
 uri = "neo4j://localhost:7687"
-username = ""
-password = ""
-driver = GraphDatabase.driver(uri, auth=(username, password))
+user = "neo4j"
+password = "neo4j"
+driver = GraphDatabase.driver(uri, auth=(user, password))
 
-# Create nodes and relationships in Neo4j
-with driver.session() as session:
-    for track_id in song_df.iterrows():
-        session.write_transaction(create_song_nodes, row)
+
+# Function to recommend similar songs for a given song
+def recommend_similar_songs(track_id):
+    with driver.session() as session:
+        # Cypher query to find similar songs for a given song
+        query = (
+            "MATCH (s1:Track {track_id: $track_id})-[:SIMILAR_TO]->(s2:Track) "
+            "RETURN s2.track_name AS similar_song, s2.artists AS artist "
+            "LIMIT 5"
+        )
+        result = session.run(query, track_id=track_id)
+        recommendations = [(record["similar_song"], record["artist"]) for record in result]
+        return recommendations
+
+# usage
+track = "3qehmGY1DnlBI5W9iFlUhR"
+recs = recommend_similar_songs(track)
+print("Recommended songs for", track, "are: ", recs)
+
 
 # Close the Neo4j driver
 driver.close()
